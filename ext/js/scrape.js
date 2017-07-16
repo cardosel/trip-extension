6081070d480b26723f3530761950ec43cca3c768
Selena Cardona <cardo23s@mtholyoke.edu>
2017-07-15 20:04:12 -0400

    shifting files

---
+++ ext/js/scrape.js
@@ -0,0 +1,15 @@
+const Xray = require('x-ray');
+const request = require('request');
+const fs = require('fs');
+
+const xray = new Xray();
+const URL = 'https://www.airbnb.com/rooms/798483';
+
+	xray(URL, 'title')(function(err, title){
+ 		
+ 		console.log(title) // returns title of airbnb
+
+ 		fs.appendFile('airbnb.json', '\n' + title); // updates json
+
+	
+	});
